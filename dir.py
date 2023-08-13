3204ea671b129f37faca1745ab860d5a0416c8c2
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-08-13 22:47:41 +0300

    dir

---
+++ dir.py
@@ -0,0 +1,15 @@
+
+
+class Dir:
+
+    def __init__(self, absolute_path: str):
+        self.absolute_path: str = absolute_path
+
+    def read(self):
+
+
+# class Picture:
+#
+#     def __init__(self):
+#         pass
+
